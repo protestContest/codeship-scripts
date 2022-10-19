b026ef321e910694668720199191f5c759843d24
Zack Michener <z@zjm.me>
2022-10-19 13:00:42 -0700

    Update elixir.sh

--- languages/elixir.sh
+++ languages/elixir.sh
@@ -10,6 +10,7 @@
 #
 # Include in your builds via
 # source /dev/stdin <<< "$(curl -sSL https://raw.githubusercontent.com/codeship/scripts/master/languages/elixir.sh)"
+OTP_VERSION=${OTP_VERSION:="25"}
 ELIXIR_VERSION=${ELIXIR_VERSION:="1.2.3"}
 ELIXIR_PATH=${ELIXIR_PATH:=$HOME/elixir}
 CACHED_DOWNLOAD="${HOME}/cache/elixir-v${ELIXIR_VERSION}.zip"
@@ -18,7 +19,9 @@ CACHED_DOWNLOAD="${HOME}/cache/elixir-v${ELIXIR_VERSION}.zip"
 # would cause an infrastructure error message on Codeship.
 mkdir -p "${ELIXIR_PATH}"
 
-wget --continue --output-document "${CACHED_DOWNLOAD}" "https://github.com/elixir-lang/elixir/releases/download/v${ELIXIR_VERSION}/Precompiled.zip"
+ELIXIR_DOWNLOAD_URL="https://github.com/elixir-lang/elixir/releases/download/v${ELIXIR_VERSION}/elixir-otp-${OTP_VERSION}.zip"
+
+wget --continue --output-document "${CACHED_DOWNLOAD}" "${ELIXIR_DOWNLOAD_URL}"
 unzip -q -o "${CACHED_DOWNLOAD}" -d "${ELIXIR_PATH}"
 
 export PATH="${ELIXIR_PATH}/bin:${PATH}"
